2f22100a3c42120b3e2e4a88d881c1396b3b712f
sherly-23 <sherlyyamarthy@gmail.com>
2025-04-09 15:23:34 +0530

    java file

---
+++ Add.java
@@ -0,0 +1,10 @@
+package java1;`
+    public class Add {
+        public static void main(String[] args)
+        {
+            int a=10;
+            int b=5;
+            int sum=a+b;
+            System.out.println("Addition:"+sum);
+        }
+    }
